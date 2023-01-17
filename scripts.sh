ce8d227142b45b3c8f7dbb051afc9d0bea262288
Oliver José Tzunún Dávila <olstertecn597@outlook.com>
2023-01-17 10:14:37 -0600

    Create scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,5 @@
+yay -S picom-git awesome-git acpid git mpd ncmpcpp wmctrl \
+firefox lxappearance gucharmap thunar alacritty neovim polkit-gnome \
+xdotool xclip scrot brightnessctl alsa-utils pulseaudio jq acpi rofi \
+inotify-tools zsh mpdris2 bluez bluez-utils bluez-plugins acpi acpi_call \
+playerctl redshift cutefish-cursor-themes-git cutefish-icons upower xorg xorg-init tar
