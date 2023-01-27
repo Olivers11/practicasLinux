578979be25f836cb15a649628ae1fe952ff80df7
Oliver José Tzunún Dávila <olstertecn597@outlook.com>
2023-01-27 11:22:06 -0600

    Delete scripts.sh

--- scripts.sh
+++
@@ -1,5 +0,0 @@
-yay -S picom-git awesome-git acpid git mpd ncmpcpp wmctrl \
-firefox lxappearance gucharmap thunar alacritty neovim polkit-gnome \
-xdotool xclip scrot brightnessctl alsa-utils pulseaudio jq acpi rofi \
-inotify-tools zsh mpdris2 bluez bluez-utils bluez-plugins acpi acpi_call \
-playerctl redshift cutefish-cursor-themes-git cutefish-icons upower xorg xorg-init tar
